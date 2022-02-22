a6a36b291b47ea215ed7589b3e088126d37d0484
Thiago Rodrigues <thiagor_@live.com>
2022-02-22 19:09:33 -0300

    refactor: dates using ISO-8601

--- src/main/kotlin/dev/trodrigues/ead/course/controllers/responses/CourseResponse.kt
+++ src/main/kotlin/dev/trodrigues/ead/course/controllers/responses/CourseResponse.kt
@@ -1,12 +1,12 @@
 package dev.trodrigues.ead.course.controllers.responses
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import dev.trodrigues.ead.course.enums.CourseLevel
 import dev.trodrigues.ead.course.enums.CourseStatus
 import java.time.LocalDateTime
 import java.util.*
 
 data class CourseResponse(
-
     val id: UUID,
     val name: String,
     val description: String,
@@ -14,7 +14,8 @@ data class CourseResponse(
     val courseLevel: CourseLevel,
     val userInstructor: UUID,
     val imageUrl: String? = null,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     val creationDate: LocalDateTime? = null,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     val lastUpdateDate: LocalDateTime? = null
-
 )
--- src/main/kotlin/dev/trodrigues/ead/course/controllers/responses/LessonResponse.kt
+++ src/main/kotlin/dev/trodrigues/ead/course/controllers/responses/LessonResponse.kt
@@ -1,5 +1,6 @@
 package dev.trodrigues.ead.course.controllers.responses
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 import java.util.*
 
@@ -7,5 +8,6 @@ data class LessonResponse(
     val id: UUID,
     val title: String,
     val description: String? = null,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     val creationDate: LocalDateTime
 )
