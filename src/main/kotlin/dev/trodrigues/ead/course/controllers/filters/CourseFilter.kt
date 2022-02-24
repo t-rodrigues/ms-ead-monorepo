849e14e35f34b734b733b9ed83f1187520d64c5b
Thiago Rodrigues <thiagor_@live.com>
2022-02-23 21:14:50 -0300

    feat: add courseUser search

--- src/main/kotlin/dev/trodrigues/ead/course/controllers/filters/CourseFilter.kt
+++ src/main/kotlin/dev/trodrigues/ead/course/controllers/filters/CourseFilter.kt
@@ -2,9 +2,11 @@ package dev.trodrigues.ead.course.controllers.filters
 
 import dev.trodrigues.ead.course.enums.CourseLevel
 import dev.trodrigues.ead.course.enums.CourseStatus
+import java.util.*
 
 data class CourseFilter(
     val name: String?,
     val courseStatus: CourseStatus?,
-    val courseLevel: CourseLevel?
+    val courseLevel: CourseLevel?,
+    val userId: UUID,
 )
--- src/main/kotlin/dev/trodrigues/ead/course/specifications/CourseSpec.kt
+++ src/main/kotlin/dev/trodrigues/ead/course/specifications/CourseSpec.kt
@@ -2,7 +2,9 @@ package dev.trodrigues.ead.course.specifications
 
 import dev.trodrigues.ead.course.controllers.filters.CourseFilter
 import dev.trodrigues.ead.course.models.CourseModel
+import dev.trodrigues.ead.course.models.CourseUserModel
 import org.springframework.data.jpa.domain.Specification
+import java.util.*
 import javax.persistence.criteria.Predicate
 
 object CourseSpec {
@@ -10,6 +12,13 @@ object CourseSpec {
     fun courses(filter: CourseFilter): Specification<CourseModel> {
         return Specification { root, query, criteriaBuilder ->
             val predicates = mutableListOf<Predicate>()
+
+            filter.userId?.let {
+                val courseProd = root.join<CourseModel, CourseUserModel>("coursesUsers")
+                predicates.add(
+                    criteriaBuilder.equal(courseProd.get<UUID>("userId"), it)
+                )
+            }
 
             filter.name?.let {
                 predicates.add(
