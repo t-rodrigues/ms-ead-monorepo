8a03c484175bbb6d6adba9271e974cc8ea081781
Thiago Rodrigues <thiagor_@live.com>
2022-02-25 22:34:47 -0300

    refactor: rename CourseClient to AuthUserClient and improve application.yml

--- src/main/kotlin/dev/trodrigues/ead/course/clients/AuthUserClient.kt
+++ src/main/kotlin/dev/trodrigues/ead/course/clients/AuthUserClient.kt
@@ -5,13 +5,17 @@ import dev.trodrigues.ead.course.controllers.responses.UserResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import java.util.*
 
-@FeignClient(name = "users", url = "\${users.url}")
-interface CourseClient {
+@FeignClient(name = "users", url = "\${ead.api.url.auth-user}")
+interface AuthUserClient {
 
     @GetMapping("/users")
     fun getUsersByCourse(@RequestParam courseId: UUID, pageable: Pageable): PageResponse<UserResponse>
+
+    @GetMapping("/users/{userId}")
+    fun getUserById(@PathVariable userId: UUID): UserResponse
 
 }
