df9bd5edb659cee10d8e511c646d158b75509669
Thiago Rodrigues <thiagor_@live.com>
2022-03-01 13:34:41 -0300

    feat: enable eureka client

--- src/main/kotlin/dev/trodrigues/ead/authuser/AuthUserApplication.kt
+++ src/main/kotlin/dev/trodrigues/ead/authuser/AuthUserApplication.kt
@@ -2,8 +2,10 @@ package dev.trodrigues.ead.authuser
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 class AuthUserApplication
