eddc0f5d4635c5dbd1c35f511c9ae0b8aca3cb56
Thiago Rodrigues <thiagor_@live.com>
2022-03-07 20:56:34 -0300

    feat: add Retry and fallback

--- src/main/kotlin/dev/trodrigues/ead/authuser/controllers/UserCourseController.kt
+++ src/main/kotlin/dev/trodrigues/ead/authuser/controllers/UserCourseController.kt
@@ -3,7 +3,10 @@ package dev.trodrigues.ead.authuser.controllers
 import dev.trodrigues.ead.authuser.clients.CourseClient
 import dev.trodrigues.ead.authuser.controllers.responses.CourseResponse
 import dev.trodrigues.ead.authuser.controllers.responses.PageResponse
+import dev.trodrigues.ead.authuser.extension.toPageResponse
 import dev.trodrigues.ead.authuser.services.UserService
+import io.github.resilience4j.retry.annotation.Retry
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -20,12 +23,17 @@ class UserCourseController(
 ) {
 
     @GetMapping("/users/{userId}/courses")
+    @Retry(name = "retryInstance", fallbackMethod = "fallback")
     fun getCoursesByUser(
         @PathVariable userId: UUID,
         @PageableDefault(size = 10, sort = ["creationDate"]) pageable: Pageable
     ): PageResponse<CourseResponse> {
         val user = userService.findById(userId)
         return courseClient.getCoursesByUser(user.id!!, pageable)
+    }
+
+    fun fallback(userId: UUID, pageable: Pageable, throwable: Throwable): PageResponse<CourseResponse> {
+        return PageImpl(mutableListOf<CourseResponse>()).toPageResponse()
     }
 
 }
