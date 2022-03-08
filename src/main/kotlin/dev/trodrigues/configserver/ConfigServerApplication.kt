01cc92fc170f87cf2928af98ba054054ab1b46eb
Thiago Rodrigues <thiagor_@live.com>
2022-03-08 00:17:32 -0300

    chore: add git config

--- src/main/kotlin/dev/trodrigues/configserver/ConfigServerApplication.kt
+++ src/main/kotlin/dev/trodrigues/configserver/ConfigServerApplication.kt
@@ -5,6 +5,8 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 class ConfigServerApplication
 
